e3f6863c713da0971a13665af9ceea60a250361b
CAPPAndrew <36282591+CAPPAndrew@users.noreply.github.com>
2018-05-26 15:40:13 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -6,46 +6,46 @@ import logging
 
 
 class Connect():
-    def __init__(self):
-        self.conn = None
-        self.cur = None
-        
-        self.connect_to()
-    
+	def __init__(self):
+		self.conn = None
+		self.cur = None
+		
+		self.connect_to()
+	
 
-    def connect_to(self):
-        
-        params = {'dbname': 'jocodssg',
-            'user': 'jocodssg_students',
-            'host': 'postgres.dssg.io',
-            'password': 'aibaecighoobeeba',
-            'port': 5432}
+	def connect_to(self):
+		
+		params = {'dbname': 'jocodssg',
+			'user': 'jocodssg_students',
+			'host': 'postgres.dssg.io',
+			'password': 'aibaecighoobeeba',
+			'port': 5432}
 
-        try:
-            self.conn = psycopg2.connect(**params)
-            print("Connected successfully.")
-        except Exception as e:
-            print("Failed to connect.")
-            print(e)
+		try:
+			self.conn = psycopg2.connect(**params)
+			print("Connected successfully.")
+		except Exception as e:
+			print("Failed to connect.")
+			print(e)
 
-        self.conn = self.conn.cursor()
-        print("Open Connection")
+		self.conn = self.conn.cursor()
+		print("Open Connection")
 
 
-    def close_connect(self):
-        if self.conn.closed == 0:
-            try:
-                self.cur.close()
-                self.conn.close()
-                print("Closed connection.")
-            except Exception as e:
-                print("Failed to close connection.")
-                print(e)
+	def close_connect(self):
+		if self.conn.closed == 0:
+			try:
+				self.cur.close()
+				self.conn.close()
+				print("Closed connection.")
+			except Exception as e:
+				print("Failed to close connection.")
+				print(e)
 		else:
 			print("No Connection")
 
 	def print_df(self, type, input_string):
-        if self.conn.closed == 0:
+		if self.conn.closed == 0:
 			if type = 'table':
 				df = pd.read_sql_table(input_string, self.conn)
 			elif type = 'query':
