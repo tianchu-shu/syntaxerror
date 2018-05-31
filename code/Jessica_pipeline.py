cf9d80b18874f0df3e4938d58a6bc532b87f6ea9
Jessica Song <sjessicas@uchicago.edu>
2018-05-31 12:52:46 -0500

    other fns

--- code/Jessica_pipeline.py
+++ code/Jessica_pipeline.py
@@ -199,7 +199,7 @@ def to_datetime(df, cols, index=-8, format="%Y%m%d"):
 # CUTTING & MERGING DATAFRAME WITH DATETIME RANGE 
 def restrain_datetime(df, date_col='arrest_date', from_date=(2010,1,1), to_date=(2015,12,31)):
 	df.index = df[date_col]
-    	df = df.sort_index()
+    df = df.sort_index()
 	df = df[datetime(*from_date):datetime(*to_date)]
 	df = df.reset_index(drop=True)
 
@@ -208,11 +208,11 @@ def restrain_datetime(df, date_col='arrest_date', from_date=(2010,1,1), to_date=
 
 
 # FILTER WITH FREQUENCY OF RE-ENTRY
-def within_frame(df, id_num='mni_no', timestamp='release_date_y', duration=365):
+def within_frame(df, id_num='mni_no', timestamp='release_date_y', col_name='re-enter-days', duration=365):
     df = df.sort_values(by=[id_num, timestamp])
-    df['re-enter-days'] = df.groupby(id_num)[timestamp].diff()
-    df['re-enter-days'] = df['re-enter-days'].apply(lambda x: x.days)
-    df['within_{}'.format(duration)] = np.where(df['re-enter-days']>duration, 1, 0)
+    df['{}'.format(col_name)] = df.groupby(id_num)[timestamp].diff()
+    df['{}'.format(col_name)] = df['{}'.format(col_name)].apply(lambda x: x.days)
+    df['within_{}'.format(duration)] = np.where(df['{}'.format(col_name)]>duration, 1, 0)
 
     
     return df
@@ -365,6 +365,23 @@ def create_dummy(df, columns):
 
 	return df
 
+
+
+# CHANGE IN VARS FOR EACH
+def is_changed(df, id_num='mni_no', var='mar_stat'):
+	df['{}_changed'.format(var)] = (df[var]!=df[var].shift()) | (df[id_num]!=df[id_num].shift())
+
+	return df
+	
+
+
+# CUM SUM OF VARS
+def cum_vals(df, id_num='mni_no', var='re-enter-days'):
+
+	df = df.sort_values(by=[id_num, var])
+	df['{}_so_far'.format(var)] = df.groupby(id_num)[var].cumsum()
+
+	return df
 
 
 
