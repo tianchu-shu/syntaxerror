71dd3f3004ebacd440ff6873c5d983405814d1f2
CAPPAndrew <36282591+CAPPAndrew@users.noreply.github.com>
2018-05-31 16:15:57 -0500

    Update load_dfs.py

--- code/load_dfs.py
+++ code/load_dfs.py
@@ -5,7 +5,7 @@ def load_df(table_list):
 	conn = connection.Connect()
 	for x in table_list:
 		file_name = x + "_df"
-		df_dict[file_name]= connection.print_df('table', x)
+		df_dict[file_name] = connection.print_df('table', x)
 	
 	return df_dict
 	
