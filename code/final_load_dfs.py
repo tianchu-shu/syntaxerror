3a13678448688749bac39eb75b5a7b5c8799cf76
CAPPAndrew <36282591+CAPPAndrew@users.noreply.github.com>
2018-06-01 12:10:16 -0500

    Update and rename load_dfs.py to final_load_dfs.py

---
+++ code/final_load_dfs.py
@@ -0,0 +1,34 @@
+import connection
+
+def load_from_file(dataframe_string, row_num = None):
+	'''
+	Function reads the .csv file into a panda dataframe.
+	Inputs:
+	dataframe_string: String for file name
+	filetype: string for file_type
+	row_num: integer, number of rows to read if excel or csv
+	Outputs:
+	df = A panda dataframe
+	'''
+	if '.csv' in dataframe_string:
+		df = pd.read_csv(dataframe_string, nrows = row_num)
+	elif '.xls' in dataframe_string:
+		df = pd.read_excel(dataframe_string, nrows = row_num)
+	elif '.json' in dataframe_string:
+		df = pd.read_json(dataframe_string)
+	
+	print("Loaded" + dataframe_string)
+	
+	return df
+
+def load_from_db(table_list):
+	df_dict = {}
+	conn = connection.Connect()
+	for x in table_list:
+		file_name = x + "_df"
+		df_dict[file_name] = connection.return_df('table', x)
+		print("Loaded" + file_name)
+	
+	return df_dict
+	
+	
