2a6c74dacadd76b0b9e385112b9c112833c41aff
Amir Akhter Kazi <35741606+amirkazi@users.noreply.github.com>
2018-06-03 15:45:57 -0500

    Update final_classifier_final.py

--- code/final_classifier_final.py
+++ code/final_classifier_final.py
@@ -135,3 +135,23 @@ def best_grid(rdf, method = "auc-roc"):
     return best
 
 
+def finding_risk_scores(x_train, x_test, y_train, y_test, grid):
+    '''
+    Adds the y-pred probs for each model to the x_test
+    Can be used to find top X% of people at risk according to any given model
+    '''
+    models = []
+    for row in grid:
+        models.append(row)
+    
+    x_test_copy = x_test.copy()
+    for index, clf in enumerate([clfs[x] for x in models]):
+        model_params = grid[models[index]]
+        for p in ParameterGrid(model_params):
+                clf.set_params(**p)
+                y_pred_probs = clf.fit(x_train, y_train).predict_proba(x_test)[:,1]
+                name = str(models[index])
+                print (name)
+                x_test_copy[name] = y_pred_probs
+                
+    return x_test_copy
