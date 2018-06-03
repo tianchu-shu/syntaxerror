8e4da576a2472961c688bfd9e424b336033db961
Tianchu <35506630+tianchu-shu@users.noreply.github.com>
2018-06-03 15:08:54 -0500

    Add files via upload

---
+++ code/final_classifier_final.py
@@ -0,0 +1,92 @@
+clfs = {'RF': RandomForestClassifier(),
+    'Boost': AdaBoostClassifier(DecisionTreeClassifier(max_depth=1)),
+    'Logit': LogisticRegression(),
+    'SVM': SVC(probability=True, random_state=0),
+    'Tree': DecisionTreeClassifier(),
+    'Bag': BaggingClassifier(),
+    'KNN': KNeighborsClassifier(),
+    'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, criterion='entropy'),
+    'NB': GaussianNB()}
+
+
+small_grid = {'RF':{'n_estimators': [10,100], 'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10]},
+    'Boost': {'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000]},
+    'Logit': {'penalty': ['l1','l2'], 'C': [0.001,0.01,0.1,1,10]},
+    'SVM' :{'C' :[0.01,0.1,1,10],'kernel':['linear']},
+    'Tree': {'criterion': ['gini', 'entropy'], 'max_depth': [5,10,20,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5]},
+    'Bag':{'n_estimators':[1,10,20,50], 'max_samples':[5,10], 'max_features': [5,10]},
+    'KNN' :{'n_neighbors': [1,10,50],'weights': ['uniform','distance'],'algorithm': ['auto']},
+    'ET': {'n_estimators': [10,100], 'criterion' : ['gini', 'entropy'] ,'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10], 'n_jobs': [-1]},
+    'NB' : {}}
+
+
+def generate_binary_at_k(y_scores, k):
+    '''
+    Set first k% as 1, the rest as 0.
+    '''
+    cutoff_index = int(len(y_scores) * (k / 100.0))
+    test_predictions_binary = [1 if x < cutoff_index else 0 for x in range(len(y_scores))]
+    return test_predictions_binary
+
+
+def scores_at_k(y_true, y_scores, k):
+    '''
+    For a given level of k, calculate corresponding
+    precision, recall, and f1 scores.
+    '''
+    preds_at_k = generate_binary_at_k(y_scores, k)
+    precision = round(precision_score(y_true, preds_at_k),4)
+    recall = round(recall_score(y_true, preds_at_k), 4)
+    f1 = round(f1_score(y_true, preds_at_k),4)
+    return precision, recall, f1
+
+
+def plot(y_test, y_pred_probs, model, para = None):
+
+    precision, recall, _ = precision_recall_curve(y_test, y_pred_probs)
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve for {} model: AUC={:.2f} \n with parameters: {}'.\
+                        format(model, average_precision_score(y_test, y_pred_probs), para))
+    plt.show()
+
+
+def clf_loop(models, x_train, x_test, y_train, y_test, grid=small_grid):
+    '''
+    Run model with given params on x and y
+    and print out the scores for comparison
+    '''
+    results_df = pd.DataFrame(columns=('model_type', 'parameters', 'auc-roc', 'precision', 'time', 'precision,recall,f1 at_5',
+     'precision,recall,f1 at_10', 'precision,recall,f1 at_20', 'precision,recall,f1 at_30', 'precision,recall,f1 at_50'))
+    for index, clf in enumerate([clfs[x] for x in models]):
+        model_params = grid[models[index]]
+        for p in ParameterGrid(model_params):
+            try:
+                start = time.time()
+                clf.set_params(**p)
+                y_pred_probs = clf.fit(x_train, y_train).predict_proba(x_test)[:,1]
+                accuracy = clf.score(x_test, y_test)
+                end = time.time()
+                #Zip, unzip to ensure corresponding order
+                y_pred_probs_sorted, y_test_sorted = zip(*sorted(zip(y_pred_probs, y_test), reverse=True))
+
+                results_df.loc[len(results_df)] = [models[index], p,
+                                             round(roc_auc_score(y_test, y_pred_probs),4),
+                                             round(average_precision_score(y_test, y_pred_probs),4),
+                                             round(end-start, 4) ,                                           
+                                             scores_at_k(y_test_sorted,y_pred_probs_sorted,5.0),
+                                             scores_at_k(y_test_sorted,y_pred_probs_sorted,10.0),
+                                             scores_at_k(y_test_sorted,y_pred_probs_sorted,20.0),
+                                             scores_at_k(y_test_sorted,y_pred_probs_sorted,30.0),
+                                             scores_at_k(y_test_sorted,y_pred_probs_sorted,50.0)]
+
+                #plot(y_test, y_pred_probs, models[index],p)
+                
+            except IndexError as e:
+                print(e)
+                continue
+    return results_df
