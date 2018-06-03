d0f35e7c5c6fe53318ea3797fd6f811d033408e7
Jessica Song <sjeeyoon@gmail.com>
2018-06-03 09:25:39 -0500

    Update final_temporal.py

--- code/final_temporal.py
+++ code/final_temporal.py
@@ -23,11 +23,35 @@ def restrain_datetime(df, date_col='arrest_date', from_date=(2010,1,1), to_date=
 
 
 
-# FILTER WITH FREQUENCY OF RE-ENTRY
+# FILTER WITH FREQUENCY OF RE-ENTRY (PREV BOOKING DATE ~ NEXT BOOKING DATE)
 def within_frame(df, id_num='dedupe_id', timestamp='booking_date', col_name='re-entry', duration=365):
 	df = df.sort_values(by=[id_num, timestamp])
 	df['{}'.format(col_name)] = df.groupby[id_num][timestamp].diff()
 	df['{}'.format(col_name)] = df['{}'.format(col_name)].apply(lambda x: x.days)
 	df['within_{}'.format(duration)] = np.where(df['{}'.format(col_name)]>duration, 1, 0)    
 	
+	return df
+
+# FILTER WITH FREQUENCY OF RE-ENTRY (PREV RELEASED DATE ~ NEXT BOOKING DATE)
+def within_frame2(df, id='dedupe_id', col1='booking_date', col2='release_date', days=365, windows=[0.5,1,2]):
+	
+	# Change to datetime
+	df[col1] = pd.to_datetime(df[col1])
+	df[col2] = pd.to_datetime(df[col2])
+
+	df['after_prev_booked'] = df.groupby(id)[date_col].diff()
+	df['stayed'] = df[col2] - df[col1]
+
+	df = df[df['after_prev_booked'].notnull()]
+
+	df['after_released'] = df['after_prev_booked'] - df['stayed']
+	# Convert the datetime type to integer
+	df['after_released'] = df['after_released'].astype('timedelta64[D]')
+
+
+	for window in windows:
+		within = int(window*days)
+		df['within_{}days'.format(within)] = np.where(df['after_released'] <= within, 1, 0)
+
+
 	return df
