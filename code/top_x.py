9dc99b7b49fa634a2014a9b81533a102789f10c4
Amir Akhter Kazi <35741606+amirkazi@users.noreply.github.com>
2018-06-03 13:26:25 -0500

    Update top_x.py

--- code/top_x.py
+++ code/top_x.py
@@ -8,9 +8,7 @@ def finding_risk_scores(models, x_train, x_test, y_train, y_test, grid=best):
     Adds the y-pred probs for each model to the x_test
     Can be used to find top X% of people at risk according to any given model
     '''
-    #new_df = pd.DataFrame(columns = ('Bag', 'Boost', 'ET', 'KNN', 'Logit', 'NB', 'RF', 'Tree'))
-    results_df = pd.DataFrame(columns=('model_type', 'parameters', 'auc-roc', 'precision', 'time', 'precision,recall,f1 at_5',
-     'precision,recall,f1 at_10', 'precision,recall,f1 at_20', 'precision,recall,f1 at_30', 'precision,recall,f1 at_50'))
+    
     x_test_copy = x_test.copy()
     for index, clf in enumerate([clfs[x] for x in models]):
         model_params = grid[models[index]]
