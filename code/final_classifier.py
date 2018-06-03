d6ed6ae7f40ad73e4ab57e3164cf57135cf3211f
CAPPAndrew <36282591+CAPPAndrew@users.noreply.github.com>
2018-06-03 15:01:20 -0500

    Update final_classifier.py

--- code/final_classifier.py
+++ code/final_classifier.py
@@ -18,6 +18,76 @@ import seaborn as sns
 import datetime
 
 
+
+MODELS = ['RF', 'ET', 'AB', 'LR', 'SVM', 'GB', 'NB', 'DT', 'SGD', 'KNN']
+
+CLFS = {'RF': RandomForestClassifier(n_estimators=50, n_jobs=-1),
+        'ET': ExtraTreesClassifier(n_estimators=10, n_jobs=-1, criterion='entropy'),
+        'AB': AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=200),
+        'LR': LogisticRegression(penalty='l1', C=1e5),
+        'SVM': svm.SVC(kernel='linear', probability=True, random_state=0),
+        'GB': GradientBoostingClassifier(learning_rate=0.05, subsample=0.5, max_depth=6, n_estimators=10),
+        'NB': GaussianNB(),
+        'DT': DecisionTreeClassifier(),
+        'SGD': SGDClassifier(loss="hinge", penalty="l2"),
+        'KNN': KNeighborsClassifier(n_neighbors=3) 
+            }
+
+
+
+FEATURE_CLFS = {'RandomForestClassifier':'feature_importances',
+        'ExtraTreesClassifier': 'feature_importances',
+        'AdaBoostClassifier': 'feature_importances',
+        'LogisticRegression': 'coef',
+        'svm.SVC': 'coef',
+        'GradientBoostingClassifier': 'feature_importances',
+        'GaussianNB': None,
+        'DecisionTreeClassifier': 'feature_importances',
+        'SGDClassifier': 'coef',
+        'KNeighborsClassifier': None,
+        'linear.SVC': 'coef'}
+
+            
+LARGE_GRID = { 
+'RF':{'n_estimators': [1,10,100,1000,10000], 'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10], 'n_jobs': [-1]},
+'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.0001,0.001,0.01,0.1,1,10]},
+'SGD': { 'loss': ['hinge','log','perceptron'], 'penalty': ['l2','l1','elasticnet']},
+'ET': { 'n_estimators': [1,10,100,1000,10000], 'criterion' : ['gini', 'entropy'] ,'max_depth': [1,5,10,20,50,100], 'max_features': ['sqrt','log2'],'min_samples_split': [2,5,10], 'n_jobs': [-1]},
+'AB': { 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
+'GB': {'n_estimators': [1,10,100,1000,10000], 'learning_rate' : [0.001,0.01,0.05,0.1,0.5],'subsample' : [0.1,0.5,1.0], 'max_depth': [1,3,5,10,20,50,100]},
+'NB' : {},
+'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100],'min_samples_split': [2,5,10]},
+'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
+'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
+       }
+
+SMALL_GRID = { 
+'RF':{'n_estimators': [10,100], 'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10], 'n_jobs': [-1]},
+'LR': { 'penalty': ['l1','l2'], 'C': [0.00001,0.001,0.1,1,10]},
+'SGD': { 'loss': ['hinge','log','perceptron'], 'penalty': ['l2','l1','elasticnet']},
+'ET': { 'n_estimators': [10,100], 'criterion' : ['gini', 'entropy'] ,'max_depth': [5,50], 'max_features': ['sqrt','log2'],'min_samples_split': [2,10], 'n_jobs': [-1]},
+'AB': { 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [1,10,100,1000,10000]},
+'GB': {'n_estimators': [10,100], 'learning_rate' : [0.001,0.1,0.5],'subsample' : [0.1,0.5,1.0], 'max_depth': [5,50]},
+'NB' : {},
+'DT': {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20,50,100],'min_samples_split': [2,5,10]},
+'SVM' :{'C' :[0.00001,0.0001,0.001,0.01,0.1,1,10],'kernel':['linear']},
+'KNN' :{'n_neighbors': [1,5,10,25,50,100],'weights': ['uniform','distance'],'algorithm': ['auto','ball_tree','kd_tree']}
+       }
+
+TEST_GRID = { 
+'RF':{'n_estimators': [1], 'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+'LR': { 'penalty': ['l1'], 'C': [0.01]},
+'SGD': { 'loss': ['perceptron'], 'penalty': ['l2']},
+'ET': { 'n_estimators': [1], 'criterion' : ['gini'] ,'max_depth': [1], 'max_features': ['sqrt'],'min_samples_split': [10]},
+'AB': { 'algorithm': ['SAMME'], 'n_estimators': [1]},
+'GB': {'n_estimators': [1], 'learning_rate' : [0.1],'subsample' : [0.5], 'max_depth': [1]},
+'NB' : {},
+'DT': {'criterion': ['gini'], 'max_depth': [1],'min_samples_split': [10]},
+'SVM' :{'C' :[0.01],'kernel':['linear']},
+'KNN' :{'n_neighbors': [5],'weights': ['uniform'],'algorithm': ['auto']}
+       }
+
+
 def generate_binary_at_k(y_scores, k):
     '''
     Set first k% as 1, the rest as 0.
