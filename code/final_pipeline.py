ab0769a3e61f0f6a4aadc40ef37bcb51c2665e94
Jessica Song <sjessicas@uchicago.edu>
2018-06-05 14:05:15 -0500

    need plot mult

--- code/final_pipeline.py
+++ code/final_pipeline.py
@@ -471,6 +471,49 @@ def feature_importance(x_train, y_train, bestm, x="ET", k=10):
     plt.show()
 
 
+
+
+def plot_mult(models, x_train, x_test, y_train, y_test, bestm):
+    '''
+    Run model with the best given params on x and y
+    and print out all the best models' on the same graph
+    '''
+    k = len(models)
+    current_palette = sns.color_palette(sns.color_palette("Paired", k))
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel('percent of population')
+    ax1.set_ylabel('precision', color='#3498db')
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('recall', color="#e74c3c")
+    for index, clf in enumerate([CLFS[x] for x in models]):
+        model_params = bestm[models[index]]
+        for p in ParameterGrid(model_params):
+            try:
+                clf.set_params(**p)
+                y_pred_probs = clf.fit(x_train, y_train).predict_proba(x_test)[:,1]
+                
+                precision_curve, recall_curve, pr_thresholds = precision_recall_curve(y_test, y_pred_probs)
+                precision_curve = precision_curve[:-1]
+                recall_curve = recall_curve[:-1]
+                pct_above_per_thresh = []
+                number_scored = len(y_pred_probs)
+                for value in pr_thresholds:
+                    num_above_thresh = len(y_pred_probs[y_pred_probs>=value])
+                    pct_above_thresh = num_above_thresh / float(number_scored)
+                    pct_above_per_thresh.append(pct_above_thresh)
+                pct_above_per_thresh = np.array(pct_above_per_thresh)
+                
+                ax1.plot(pct_above_per_thresh, precision_curve, c=current_palette[index])
+                ax2.plot(pct_above_per_thresh, recall_curve, c=current_palette[index])                
+            except IndexError as e:
+                print(e)
+                continue
+    ax1.legend(models)
+    plt.show()
+
+
+
+
 def baseline(df, col):
 
     return df[col].sum() / df.shape[0]
